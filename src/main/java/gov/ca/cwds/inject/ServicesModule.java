2d8c826e6b87b2c07fee3dad03c15bfe12e22989
Sergii Redchuk <serge.redchuk@engagepoint.com>
2017-05-05 17:09:56 -0700

    more clean up

--- src/main/java/gov/ca/cwds/inject/ServicesModule.java
+++ src/main/java/gov/ca/cwds/inject/ServicesModule.java
@@ -1,19 +1,9 @@
 package gov.ca.cwds.inject;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
 import com.google.inject.Provides;
-import com.google.inject.matcher.Matchers;
 
-import gov.ca.cwds.data.CmsSystemCodeSerializer;
-import gov.ca.cwds.data.persistence.cms.ApiSystemCodeCache;
-import gov.ca.cwds.data.persistence.cms.CmsSystemCodeCacheService;
-import gov.ca.cwds.rest.ApiConfiguration;
 import gov.ca.cwds.rest.services.es.IndexQueryService;
-import io.dropwizard.hibernate.HibernateBundle;
-import io.dropwizard.hibernate.UnitOfWork;
-import io.dropwizard.hibernate.UnitOfWorkAspect;
-import io.dropwizard.hibernate.UnitOfWorkAwareProxyFactory;
 
 import javax.validation.Validation;
 import javax.validation.Validator;
@@ -27,42 +17,6 @@ import javax.validation.Validator;
 public class ServicesModule extends AbstractModule {
 
   /**
-   * @author CWDS API Team
-   */
-  public static class UnitOfWorkInterceptor implements org.aopalliance.intercept.MethodInterceptor {
-
-    @Inject
-    @CmsHibernateBundle
-    HibernateBundle<ApiConfiguration> cmsHibernateBundle;
-    UnitOfWorkAwareProxyFactory proxyFactory;
-
-    @Inject
-    @NsHibernateBundle
-    HibernateBundle<ApiConfiguration> nsHibernateBundle;
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public Object invoke(org.aopalliance.intercept.MethodInvocation mi) throws Throwable {
-
-      proxyFactory =
-          UnitOfWorkModule.getUnitOfWorkProxyFactory(cmsHibernateBundle, nsHibernateBundle);
-      UnitOfWorkAspect aspect = proxyFactory.newAspect();
-      try {
-        aspect.beforeStart(mi.getMethod().getAnnotation(UnitOfWork.class));
-        Object result = mi.proceed();
-        aspect.afterEnd();
-        return result;
-      } catch (Exception e) {
-        aspect.onError();
-        throw e;
-      } finally {
-        aspect.onFinish();
-      }
-    }
-
-  }
-
-  /**
    * Default, no-op constructor.
    */
   ServicesModule() {
@@ -72,14 +26,6 @@ public class ServicesModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(IndexQueryService.class);
-
-    // Register CMS system code translator.
-    bind(ApiSystemCodeCache.class).to(CmsSystemCodeCacheService.class).asEagerSingleton();
-    bind(CmsSystemCodeSerializer.class).asEagerSingleton();
-
-    UnitOfWorkInterceptor interceptor = new UnitOfWorkInterceptor();
-    bindInterceptor(Matchers.any(), Matchers.annotatedWith(UnitOfWork.class), interceptor);
-    requestInjection(interceptor);
   }
 
   @Provides
