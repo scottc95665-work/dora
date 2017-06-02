6bc1bcd6f47c0f80438e46c58ca9a5d7c448041d
Oleksander Kuznetsov <oleksander.kuznetsov@engagepoint.com>
2017-06-02 12:24:37 -0700

    Add Dora security disabled key

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$XPACK_ENABLED" ]; then
+  echo "X-PACK disabled"
+  DORA_CONFIG="config/dora_nosec.yml"
+else
+  echo "X-PACK enabled"
+  DORA_CONFIG="config/dora.yml"
+fi
+
+java -jar dora.jar server ${dora_config}
