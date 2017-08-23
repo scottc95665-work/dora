ea2e5fb6c91009764254aab7cdd8ad7ab1ce1510
Sergii Redchuk <serge.redchuk@engagepoint.com>
2017-08-23 13:41:22 -0700

    #149272217 - Dora: Implement smoke test set for Dora: Unit Tests coverage

--- dora-api/src/test/java/gov/ca/cwds/rest/resources/SystemInformationResourceTest.java
+++ dora-api/src/test/java/gov/ca/cwds/rest/resources/SystemInformationResourceTest.java
@@ -7,8 +7,10 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 import com.squarespace.jersey2.guice.JerseyGuiceUtils;
+import gov.ca.cwds.dora.dto.HealthCheckResultDTO;
 import gov.ca.cwds.dora.dto.SystemInformationDTO;
 import gov.ca.cwds.rest.BaseDoraApplicationTest;
+import java.util.SortedMap;
 import javax.ws.rs.core.MediaType;
 import org.junit.After;
 import org.junit.Before;
@@ -30,13 +32,13 @@ public class SystemInformationResourceTest extends BaseDoraApplicationTest {
   }
 
   @Test
-  public void applicationGetReturns200() {
+  public void testApplicationGetReturns200() {
     assertThat(clientTestRule.target(SYSTEM_INFORMATION).request()
         .accept(MediaType.APPLICATION_JSON).get().getStatus(), is(equalTo(200)));
   }
 
   @Test
-  public void applicationGetReturnsV1JsonContentType() {
+  public void testApplicationGetReturnsV1JsonContentType() {
     assertThat(clientTestRule.target(SYSTEM_INFORMATION).request()
             .accept(MediaType.APPLICATION_JSON).get().getMediaType().toString(),
         is(equalTo(MediaType.APPLICATION_JSON)));
@@ -46,10 +48,33 @@ public class SystemInformationResourceTest extends BaseDoraApplicationTest {
   public void testSystemInformationGet() throws Exception {
     SystemInformationDTO systemInformationDTO = clientTestRule.target(SYSTEM_INFORMATION)
         .request(MediaType.APPLICATION_JSON).get(SystemInformationDTO.class);
+
     assertThat(systemInformationDTO.getApplicationName(), is(equalTo("CWDS Dora")));
     assertThat(systemInformationDTO.getVersion(), is(notNullValue()));
+  }
 
-    assertThat(systemInformationDTO.getHealthCheckResults(), is(notNullValue()));
-    assertThat(systemInformationDTO.getHealthCheckResults().values().size(), is(equalTo(5)));
+  @Test
+  public void testHealthChecksResults() throws Exception {
+    SystemInformationDTO systemInformationDTO = clientTestRule.target(SYSTEM_INFORMATION)
+        .request(MediaType.APPLICATION_JSON).get(SystemInformationDTO.class);
+
+    SortedMap<String, HealthCheckResultDTO> healthCheckResults = systemInformationDTO
+        .getHealthCheckResults();
+
+    assertThat(healthCheckResults, is(notNullValue()));
+    assertThat(healthCheckResults.values().size(), is(equalTo(5)));
+
+    assertHealthCheckResult(healthCheckResults.get("deadlocks"), true);
+    assertHealthCheckResult(healthCheckResults.get("dora-es-config"), true);
+
+    // there is no Elasticsearch server available while Unit Tests
+    assertHealthCheckResult(healthCheckResults.get("elasticsearch-status"), false);
+    assertHealthCheckResult(healthCheckResults.get("elasticsearch-plugin-x-pack"), false);
+    assertHealthCheckResult(healthCheckResults.get("elasticsearch-plugin-analysis-phonetic"), false);
+  }
+
+  private void assertHealthCheckResult(HealthCheckResultDTO healthCheckResultDTO, boolean isHealthy) {
+    assertThat(healthCheckResultDTO, is(notNullValue()));
+    assertThat(healthCheckResultDTO.isHealthy(), is(isHealthy));
   }
 }
