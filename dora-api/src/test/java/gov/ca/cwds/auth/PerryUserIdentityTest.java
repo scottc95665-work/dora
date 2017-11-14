25e465db18efdcdcdd0354e3ba82479af7337c1a
Ievgenii Drozd <justajavaboy@users.noreply.github.com>
2017-11-14 14:18:40 -0800

    added tests for logging related classes;

---
+++ dora-api/src/test/java/gov/ca/cwds/auth/PerryUserIdentityTest.java
@@ -0,0 +1,51 @@
+package gov.ca.cwds.auth;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.junit.ExpectedException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+/**
+ * @author CWDS TPT-2
+ */
+public class PerryUserIdentityTest {
+
+  private static final String STAFF_ID = "TEST_STAFF_ID";
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Spy
+  private PerryUserIdentity perryUserIdentity; // "Class Under Test"
+
+  @Before
+  public void initMocks() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void type() throws Exception {
+    assertThat(PerryUserIdentity.class, notNullValue());
+  }
+
+  @Test
+  public void instantiation() throws Exception {
+    assertThat(perryUserIdentity, notNullValue());
+  }
+
+  @Test
+  public void testStaffId() throws Exception {
+    assertNull(perryUserIdentity.getStaffId());
+    perryUserIdentity.setStaffId(STAFF_ID);
+    assertNotNull(perryUserIdentity.getStaffId());
+    assertEquals(STAFF_ID, perryUserIdentity.getStaffId());
+  }
+}
