702590a76bc1ece73d3e81939f4bfebf4c16b2ea
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2018-10-14 16:28:30 -0700

    CANS-559: Perry Realm plugin configuration

---
+++ x-pack-perry-realm/src/main/java/gov/ca/cwds/xpack/realm/perry/PerryRealmPlugin.java
@@ -0,0 +1,8 @@
+package gov.ca.cwds.xpack.realm.perry;
+
+import org.elasticsearch.plugins.ActionPlugin;
+import org.elasticsearch.plugins.Plugin;
+
+public class PerryRealmPlugin extends Plugin implements ActionPlugin {
+  // Nothing to do here
+}
