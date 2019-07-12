932ebf0aaccb221a96b6c2c476e4d50f039ed169
Ed Gibbs <edward_gibbs@yahoo.com>
2019-07-12 16:24:19 -0700

    Removes preint :unicorn:

--- jenkinsfile-deploy-preint-int.groovy
+++ jenkinsfile-deploy-preint-int.groovy
@@ -7,18 +7,12 @@ def githubCredentialsId = 'fa29964d-237e-4ecb-96bc-1350dda63f79'
 def deAnsibleGithubUrl = 'git@github.com:ca-cwds/de-ansible.git'
 @Field
 def envProps = [
-  'preint': [
-    'DORA_URL': 'https://dora.preint.cwds.io/'.toString(),
-    'AUTH_MODE': 'dev'.toString(),
-    'PERRY_URL': 'https://web.preint.cwds.io'.toString()
-  ],
   'integration': [
     'DORA_URL': 'https://doraapi.integration.cwds.io/'.toString(),
     'AUTH_MODE': 'integration'.toString()
   ]
 ]
 
-deploy('preint')
 deploy('integration')
 
 def deploy(envName) {
@@ -79,21 +73,16 @@ def testsStage(envName) {
     String doraUrl = envProps[envName].DORA_URL.toString()
     String authMode = envProps[envName].AUTH_MODE.toString()
     String dockerEnv = "-e DORA_URL=$doraUrl -e TEST_TYPE=smoke -e AUTH_MODE=$authMode".toString()
-    if (envName == 'preint') {
-      String perryUrl = envProps[envName].PERRY_URL.toString()
-      sh "docker run $dockerEnv -e PERRY_URL=$perryUrl cwds/dora-tests:latest"
-    } else {
-      withCredentials([
-        string(credentialsId: 'c24b6659-fd2c-4d31-8433-835528fce0d7', variable: 'SMOKE_TEST_USER'),
-        string(credentialsId: '48619eb9-4a74-4c84-bc25-81557ed9dd7d', variable: 'SMOKE_TEST_PASSWORD'),
-        string(credentialsId: 'f75da5fa-b2c8-4ca5-896a-b8a85fa30572', variable: 'SMOKE_VERIFICATION_CODE')
-      ]) {
-        sh "docker run $dockerEnv \
-          -e SMOKE_TEST_USER=$SMOKE_TEST_USER \
-          -e SMOKE_TEST_PASSWORD=$SMOKE_TEST_PASSWORD \
-          -e SMOKE_VERIFICATION_CODE=$SMOKE_VERIFICATION_CODE \
-          cwds/dora-tests"
-      }
+    withCredentials([
+      string(credentialsId: 'c24b6659-fd2c-4d31-8433-835528fce0d7', variable: 'SMOKE_TEST_USER'),
+      string(credentialsId: '48619eb9-4a74-4c84-bc25-81557ed9dd7d', variable: 'SMOKE_TEST_PASSWORD'),
+      string(credentialsId: 'f75da5fa-b2c8-4ca5-896a-b8a85fa30572', variable: 'SMOKE_VERIFICATION_CODE')
+    ]) {
+      sh "docker run $dockerEnv \
+        -e SMOKE_TEST_USER=$SMOKE_TEST_USER \
+        -e SMOKE_TEST_PASSWORD=$SMOKE_TEST_PASSWORD \
+        -e SMOKE_VERIFICATION_CODE=$SMOKE_VERIFICATION_CODE \
+        cwds/dora-tests"
     }
   }
 }
